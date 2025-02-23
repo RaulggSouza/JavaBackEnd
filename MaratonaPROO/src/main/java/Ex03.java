27bd577241227428b8f502b9c1cd8f82a87ecf77
RaulggSouza <raulggsouza@hotmail.com>
2025-02-23 16:53:07 -0300

    :sparkles: feat: Ex03 of Marathon

--- MaratonaPROO/src/main/java/Ex03.java
+++ MaratonaPROO/src/main/java/Ex03.java
@@ -1,5 +1,30 @@
+import java.util.Scanner;
+
 public class Ex03 {
     public static void main(String[] args) {
-
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+        int[][] matrix = new int[n][n];
+        int[] line = new int[n];
+        int[] col = new int[n];
+        int maior = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++){
+                matrix[i][j] = scan.nextInt();
+            }
+        }
+        for (int i = 0; i < n; i++){
+            for (int j = 0; j < n; j++) {
+                line[i] += matrix[i][j];
+                col[i] += matrix[j][i];
+            }
+        }
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                int sum = col[j] + line[i] - matrix[i][j]*2;
+                if (sum > maior) maior = sum;
+            }
+        }
+        System.out.println(maior);
     }
 }

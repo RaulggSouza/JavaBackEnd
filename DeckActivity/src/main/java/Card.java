1d6a75dc58e0af684c27d2fc2eb17a369b0c5fad
RaulggSouza <raulggsouza@hotmail.com>
2025-02-25 15:31:53 -0300

    Exercises done in class

---
+++ DeckActivity/src/main/java/Card.java
@@ -0,0 +1,31 @@
+public class Card {
+    private final Rank rank;
+    private final Suit suit;
+    private boolean faceDown;
+
+    public Card (Rank rank, Suit suit){this(rank, suit, false);}
+
+    public Card(Rank rank, Suit suit, boolean faceDown) {
+        this.rank = rank;
+        this.suit = suit;
+        this.faceDown = faceDown;
+    }
+
+    public String cardAsString() {return isFaceDown()? "XX" : rank + " " + suit;}
+
+    public Rank getRank() {
+        return rank;
+    }
+
+    public Suit getSuit() {
+        return suit;
+    }
+
+    public boolean isFaceDown() {
+        return faceDown;
+    }
+
+    public void faceUp(){faceDown = false;}
+
+    public void faceDown(){faceDown = true;}
+}
